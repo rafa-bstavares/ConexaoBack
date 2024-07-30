c1c0c07ff64e5fa4a08b2943378ae244167dff4c
rafaTavDev <rafabstavares@gmail.com>
2024-07-30 11:42:48 -0300

    mudando tempo e preco tambem no atendente ao comprara mais saldo na consulta

--- src/server.ts
+++ src/server.ts
@@ -65,7 +65,8 @@ interface ServerToClientEvents {
   precoTempoServMsg: (data: {preco: number, tempo: number}) => void,
   erroMsg: (data: {erroMsg: string}) => void,
   clienteChamando: (data: {idProfissional: number, nomeCliente: string, idCliente: number}) => void,
-  respostaAtendente: (data: {msg: string, idCliente: number, idProfissional: number}) => void
+  respostaAtendente: (data: {msg: string, idCliente: number, idProfissional: number}) => void,
+  atualizarAdm: () => void
 }
 
 
@@ -75,10 +76,15 @@ interface ClientToServerEvents {
   adicionarNaSala: (data: {room: string}) => void,
   tempoPreco: (data: {tempo: number, preco: number, room: string}) => void,
   chamarAtendente: (data: {idProfissional: number, nomeCliente: string, idCliente: number}) => void,
-  respostaChamarAtendente: (data: {msg:  string, idCliente: number, idProfissional: number}) => void
+  respostaChamarAtendente: (data: {msg:  string, idCliente: number, idProfissional: number}) => void,
+  comprouSaldoDentroConsulta: (data: {room: string}) => void 
 }
 
 io.on("connection", (socket: Socket<ClientToServerEvents, ServerToClientEvents>) => {
+
+  socket.on("comprouSaldoDentroConsulta", (data) => {
+    io.sockets.to(data.room).emit("atualizarAdm")
+  })
 
   socket.on("clientMsg", async (data: {msg: string, room: string}) => {
 
