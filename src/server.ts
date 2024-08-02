83e63e52797fceb99553ed53d4d68add36858991
rafaTavDev <rafabstavares@gmail.com>
2024-08-02 06:12:16 -0300

    cors 080

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ import { v4 as uuidv4 } from 'uuid';
 dotenv.config()
 const server = express()
 const corsOptions = {
-  origin: "*",//["https://www.conexaoastralmistica.com.br", "https://conexaoastralmistica.com.br"],
+  origin: ["https://www.conexaoastralmistica.com.br", "https://conexaoastralmistica.com.br"],
   methods: "*",
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -26,7 +26,7 @@ server.use(express.json())
 const httpServer = createServer(server)
 const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
   cors: {
-    origin: "*",//["https://www.conexaoastralmistica.com.br", "https://conexaoastralmistica.com.br"],
+    origin: ["https://www.conexaoastralmistica.com.br", "https://conexaoastralmistica.com.br"],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
